b2a6f45f15ba394dd6f04b9612c9afa0426b2790
yunerkis <yumycary@gmail.com>
2021-01-31 12:28:47 -0500

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import {Router, ActivatedRoute, RouterEvent, Event, NavigationStart} from '@angular/router';
 
+const PROTECTED_ROUTES = ["/admin"];
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+  templateUrl: `./app.html`
 })
 
-export class AppComponent {}
+export class AppComponent {
+
+  public needsNavigationChrome = false;
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((routerEvent:Event)=> {
+      if (routerEvent instanceof NavigationStart){
+        this.needsNavigationChrome = PROTECTED_ROUTES.indexOf(routerEvent.url) != -1;
+      }
+    })
+  }
+
+}
